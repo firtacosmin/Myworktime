f426f940c951a1e7140d17878409315fd92830c3
firta <cosmin.firta@crmsoftware.ro>
2017-08-31 15:48:44 +0300

    added the registration fragment to the fragment builder

--- app/src/main/java/com/fcc/myworktime/di/modules/FragmentBuildersModule.kt
+++ app/src/main/java/com/fcc/myworktime/di/modules/FragmentBuildersModule.kt
@@ -3,6 +3,7 @@ package com.fcc.myworktime.di.modules
 import com.fcc.myworktime.MainActivityFragment
 import com.fcc.myworktime.ui.login.LoginFragment
 import com.fcc.myworktime.ui.projects.ProjectsFragment
+import com.fcc.myworktime.ui.registration.RegistrationFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -16,8 +17,8 @@ abstract class FragmentBuildersModule {
     @ContributesAndroidInjector
     internal abstract fun contributeLoginFragment(): LoginFragment
 
-//    @ContributesAndroidInjector
-//    internal abstract fun contributeRegistrationUserFragment(): RegistrationUserFragment
+    @ContributesAndroidInjector
+    internal abstract fun contributeRegistrationFragment(): RegistrationFragment
 
 
     @ContributesAndroidInjector
